76ba2d948134dd0bd3e3eda36ab936c733a77d4e
Gabriel Espanhol Santos <g.santos@dock.tech>
2024-05-07 18:25:50 -0300

    fix: fixes Computer interface name for adapter

--- adapter/iComputer.go
+++ adapter/iComputer.go
@@ -1,5 +1,5 @@
 package adapter
 
-type Computer interface {
+type IComputer interface {
 	insertIntoLightningPort()
 }
--- adapter/user.go
+++ adapter/user.go
@@ -5,7 +5,7 @@ import "fmt"
 type User struct {
 }
 
-func (user *User) InsertLightningConnectorIntoComputer(computer Computer) {
+func (user *User) InsertLightningConnectorIntoComputer(computer IComputer) {
 	fmt.Println("User inserts Lightning connector into computer.")
 	computer.insertIntoLightningPort()
 }
